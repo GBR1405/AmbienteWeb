2f6b7795ce51f13c2fe688abfa2cae722662f4d2
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2024-07-19 01:22:12 -0600

    Index Terminado

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentIndex = 0;
+
+    window.scrollCarousel = function (direction) {
+        const carouselTrack = document.querySelector('.carousel-track');
+        const cardWidth = document.querySelector('.card').offsetWidth;
+        const visibleCards = 8; 
+        const totalCards = document.querySelectorAll('.card').length;
+
+        currentIndex += direction * visibleCards;
+
+        if (currentIndex < 0) {
+            currentIndex = 0;
+        } else if (currentIndex > totalCards - visibleCards) {
+            currentIndex = totalCards - visibleCards;
+        }
+
+        const offset = currentIndex * cardWidth;
+        carouselTrack.style.transform = `translateX(-${offset}px)`;
+    }
+});
